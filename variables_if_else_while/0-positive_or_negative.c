efd9b3e94414c208bf41a525f3dda7d8bc0615a8
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-20 13:49:22 +0100

    commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'positive or negative'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
