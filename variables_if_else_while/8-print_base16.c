6aac3886a8c365fa2bb4cb32702bdb29d9eba18b
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 17:11:38 +0100

    base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'base 16'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int nmbr;
+int ltr;
+for (nmbr = 0; nmbr < 10 ; nmbr++)
+{
+putchar(nmbr);    
+}
+for (ltr = 'a'; ltr < 'g'; ltr++)
+{
+putchar(ltr);
+}
+putchar('\n');
+return (0);
+}
