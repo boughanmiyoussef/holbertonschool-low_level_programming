585e58343447e27a91e72ed1774a3bb8914f2b4b
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 18:10:24 +0100

    print combo

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,17 @@ int main(void)
 {
 int left_digit;
 int right_digit;
-for (left_digit = 0 ; left_digit <=8 ; left_digit++)
+for (left_digit = 0 ; left_digit <= 8 ; left_digit++)
 {
 for (right_digit = left_digit + 1; right_digit <= 9; right_digit++)
 {
-putchar(left_digit + '0');  
-putchar(right_digit + '0'); 
-if (left_digit != 8 || right_digit != 9) 
+putchar (left_digit + '0');
+putchar (right_digit + '0');
+if (left_digit != 8 || right_digit != 9)
 {
-putchar(','); 
-putchar(' '); 
-}  
+putchar (',');
+putchar (' ');
+}
 }
 }
 return (0);
