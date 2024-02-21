f23cf78ce643aac0487ad8b5b72b42eaf5d0ef11
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 16:26:45 +0100

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * description - 'numbers'
+ *
+ * Return: always success 0
+ *
+ */
+int main(void)
+{
+int digit;
+for (digit = 1; digit < 10; digit++)
+{
+printf("%d",digit);
+}
+return (0);
+}
