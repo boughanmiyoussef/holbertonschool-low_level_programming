f01c1955967de38ba19a9114ceaa3222de9757bd
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 18:47:31 +0100

    print combo

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+int main(void) 
+{
+int num1_10, num1_1, num2_10, num2_1;
+for (num1_10 = 0; num1_10 <= 9; num1_10++)
+{
+for (num1_1 = 0; num1_1 <= 9; num1_1++)
+{
+for (num2_10 = num1_10; num2_10 <= 9; num2_10++)
+{
+for (num2_1 = (num2_10 == num1_10) ? num1_1 + 1 : 0; num2_1 <= 9; num2_1++)
+{
+putchar(num1_10 + '0');
+putchar(num1_1 + '0');
+putchar(' ');
+putchar(num2_10 + '0');
+putchar(num2_1 + '0');
+if (num1_10 < 9 || num1_1 < 9 || num2_10 < 9 || num2_1 < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return 0;
+}
