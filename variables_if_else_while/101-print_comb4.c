d6959421789344cb50fcf6bcf438edd4f9353372
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 18:30:50 +0100

    print combo

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -7,15 +7,20 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void) 
+{
 int one, two, three;
-for (one = 0; one <= 7; one++) {
-for (two = one + 1; two <= 8; two++) {
-for (three = two + 1; three <= 9; three++) {
+for (one = 0; one <= 7; one++) 
+{
+for (two = one + 1; two <= 8; two++) 
+{
+for (three = two + 1; three <= 9; three++) 
+{
 putchar(one + '0');
 putchar(two + '0');
 putchar(three + '0');
-if (one != 7 || two != 8 || three != 9) {
+if (one != 7 || two != 8 || three != 9) 
+{
 putchar(',');
 putchar(' ');
 }
