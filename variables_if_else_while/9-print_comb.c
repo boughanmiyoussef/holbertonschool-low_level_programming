8009ecec027d22ee58f67010fb137cf54d7e9bbb
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 17:42:44 +0100

    print combo

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * 
+ * description: 'print combo'
+ * 
+ * Return: Always success 0 
+*/
+int main(void)
+{
+int nbr;
+for (nbr = 0; nbr < 10; nbr++){
+putchar(nbr + '0');
+if(nbr < 9){
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return(0);
+}
+
