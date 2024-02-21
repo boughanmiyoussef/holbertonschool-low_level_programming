90a5d9e56ce6c4cba1bd7b84ac2ce2f6c6882a3b
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 16:10:23 +0100

    alphabet except q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,15 @@
  */
 int main(void)
 {
-    int alpha;
-    for (alpha = 'a'; alpha <= 'z' ; alpha++)
-    {
-        if (alpha == 'q' || alpha == 'e')
-        {
-            continue;
-        }
-        putchar (alpha);
-    }
-    putchar('\n');
-    return (0);
+int alpha;
+for (alpha = 'a'; alpha <= 'z' ; alpha++)
+{
+if (alpha == 'q' || alpha == 'e')
+{
+continue;
+}
+putchar (alpha);
+}
+putchar('\n');
+return (0);
 }
