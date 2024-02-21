6eb7ed5eb46a8880dc49a17411bc5cd44d9c8469
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 16:57:46 +0100

    reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * 
+ * main - Entry point
+ * 
+ * description - 'reverse alphabet' 
+ * 
+ * Return: always success 
+*/
+int main(void)
+{
+int tehbahpla;
+for (tehbahpla = 'z' ; tehbahpla <= 'a' ; tehbahpla--){
+putchar (tehbahpla);
+}
+putchar('\n');
+return (0);
+}
