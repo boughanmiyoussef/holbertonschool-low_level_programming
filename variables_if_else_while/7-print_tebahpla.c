53ff8c0b8aede0dea77479c5ce265995c6964f03
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-21 16:59:12 +0100

    reverse alphabet

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 int tehbahpla;
-for (tehbahpla = 'z' ; tehbahpla <= 'a' ; tehbahpla--){
+for (tehbahpla = 'z' ; tehbahpla >= 'a' ; tehbahpla--){
 putchar (tehbahpla);
 }
 putchar('\n');
