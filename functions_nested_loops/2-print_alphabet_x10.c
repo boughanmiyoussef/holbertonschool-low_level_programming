f12487baf4ef830efcaec11d8372181133d5dc70
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-22 14:05:53 +0100

    _putchar

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet ten times, each followed by a newline
+ * print_alphabet_x10 - Prints the alphabet ten times, followed by a newline
  *
  * Return: Always void.
  */
