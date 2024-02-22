22036eee37ba5f8fa7aa924bc37b5ad2e0a1f452
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-22 18:40:26 +0100

    _putchar

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check the code.
+ *
+ * @c: the character
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z' || c >= 'A' || c <= 'Z')) ? 1 : 0;
+}
