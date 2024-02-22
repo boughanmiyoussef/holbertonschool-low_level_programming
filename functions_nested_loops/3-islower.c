91b91452e8811cae103cc28b17061dc7e4b333d4
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-22 14:25:33 +0100

    _putchar

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,9 +1,12 @@
 #include "main.h"
 /**
- * int _islower( - check the code.
+ * _islower - check the code.
+ *
+ * @c: the character
  *
  * Return: Always 0.
  */
-int _islower(int c) {
-    return (c >= 'a' && c <= 'z') ? _putchar('1') : _putchar('0');
+int _islower(int c)
+{
+return ((c >= 'a' && c <= 'z') ? _putchar('1') : _putchar('0'));
 }
