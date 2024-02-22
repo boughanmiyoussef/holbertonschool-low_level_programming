153da7484b2ab30690a895b04601edb0dc32e188
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-22 21:31:54 +0100

    function

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: always
+ * main -  prints Fibonacci numbers,
+ * Return: 0
  */
 int main(void)
 {
-int i = 2;
-float a = 1;
-float b = a + 1;
-float c = a + b;
-printf("%.0f, ", a);
-printf("%.0f, ", b);
-while (i < 98)
+	long x = 1, y = 2, z;
+printf("1, 2");
+while (z < 20365011073)
 {
-i++;
-printf("%.0f", c);
-a = b;
-b = c;
-c = a + b;
-if (i < 98)
-{
-printf(", ");
-}
+z = x + y;
+printf(", %lu", z);
+x = y;
+y = z;
 }
 printf("\n");
 return (0);
