52f506a540c48e713a18ed599666585f0db71590
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-22 20:27:18 +0100

    function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+ #include "main.h"
+ /**
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ *
+ * Return: The result.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
