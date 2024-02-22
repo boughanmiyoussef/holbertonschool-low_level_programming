111c6922dc3edf58a5bef50ec2babca09b729866
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-22 19:38:34 +0100

    function

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - check the code.
+ *
+ * @n: the number
+ *
+ * Return: Always 0.
+ */
+int _abs(int n) 
+{
+if (n < 0) 
+{
+return (-n);
+} 
+else
+{
+return (n);
+}
+}
