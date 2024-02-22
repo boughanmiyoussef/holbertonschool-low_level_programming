f2f5a6dc19e93c844ae5c229f99a8aedb80bb112
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-22 20:03:45 +0100

    function

--- functions_nested_loops/9-main.c
+++ functions_nested_loops/9-main.c
@@ -10,3 +10,49 @@ int main(void)
     times_table();
     return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table.
+ */
+void times_table(void) {
+    int row, column, product;
+
+    /* Iterate through rows */
+    for (row = 0; row <= 9; row++) {
+        /* Iterate through columns */
+        for (column = 0; column <= 9; column++) {
+            /* Compute product */
+            product = row * column;
+
+            /* Print product */
+            if (column > 0) {
+                _putchar(',');
+                if (product < 10) {
+                    _putchar(' ');
+                }
+                _putchar(' ');
+            }
+            if (product >= 10) {
+                _putchar((product / 10) + '0');
+            } else {
+                _putchar(' ');
+            }
+            _putchar((product % 10) + '0');
+        }
+        /* Move to the next line */
+        _putchar('\n');
+    }
+}
