7026da9aeb70e8c3890cdad8211c83c6330e7606
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-26 12:15:20 +0100

    print numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers 
+ * Description: print numbers form 0 to 14
+ * Return: void
+*/
+void more_numbers(void)
+{
+    int abc;
+    int def;
+    for (abc = 0; abc <= 10; abc++)
+    {
+        for (def = 0; def <= 14; def++)
+        {
+            if (def > 9) {
+                _putchar((def / 10) + '0'); 
+            }
+            _putchar((def % 10) + '0'); 
+        }
+        _putchar('\n');
+    }
+}
