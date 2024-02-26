5deac18748eac6a623a2885e8783066f6426a296
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-26 10:14:20 +0100

    Is Super

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'check uppercase character '
+ *
+ * @c: the character to be checked.
+ * Return: Always 0 (Success)
+ */
+ int _isupper(int c)
+ {
+    return (c >= 'A' && c <= 'Z' ) ? 1 : 0;
+ }
