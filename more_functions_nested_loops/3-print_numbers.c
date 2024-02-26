5c7301dfbaebb0bcc8671d4e307658dc5a7f6069
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-26 13:47:47 +0100

    print diagonal

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * print_numbers - the function the print the numbers from 0 to 9
- * Description: print the numbers 
+ * Description: print the numbers
  * Return: void
 */
 void print_numbers(void)
 {
 int i;
-    for (i = 0; i <= 9; i++)
-    {
-        _putchar(i + 48);
-    }
-    _putchar('\n');
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + 48);
+}
+_putchar('\n');
 }
