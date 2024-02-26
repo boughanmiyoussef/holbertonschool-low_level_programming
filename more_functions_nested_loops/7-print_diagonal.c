406fa3aad0d92131ffac8716e2f1c599e7351dc1
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-26 13:45:33 +0100

    print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,13 +2,13 @@
 
 /**
  * print_diagonal - Draws a diagonal line in the terminal
- * @n: Number of times 
+ * @n: Number of times
  */
 void print_diagonal(int n)
 {
 if (n <= 0)
 {
-_putchar('\n'); 
+_putchar('\n');
 }
 else
 {
@@ -17,10 +17,10 @@ for (a = 0; a < n; a++)
 {
 for (b = 0; b < a; b++)
 {
-_putchar(' '); 
+_putchar(' ');
 }
-_putchar('\\'); 
-_putchar('\n'); 
+_putchar('\\');
+_putchar('\n');
 }
 }
 }
