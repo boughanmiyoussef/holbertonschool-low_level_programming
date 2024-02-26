8ab9e728d0de0bd91fffa9fa74a6e2b19b6ab001
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-26 10:18:52 +0100

    Is Super

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -7,7 +7,7 @@
  * @c: the character to be checked.
  * Return: Always 0 (Success)
  */
- int _isupper(int c)
- {
-    return (c >= 'A' && c <= 'Z' ) ? 1 : 0;
- }
+int _isupper(int c)
+{
+return ((c >= 'A' && c <= 'Z') ? 1 : 0);
+}
