4d074551a0256334465c49360c31b4437fe74f2a
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-27 13:13:32 +0100

    print number

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - print an integer
+ * @n: integer
+*/
+void print_number(int n)
+{
+unsigned int i = n;
+if (n < 0)
+{
+_putchar(45);
+i = i - 1;
+}
+if (i / 10)
+{
+print_number(i / 10);
+}
+_putchar(i % 10 + 48);
+}
