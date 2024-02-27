859321c694f9cc82870102a381a16921369376a7
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-27 11:07:37 +0100

    print triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - print triangles
+ * Description: print a triangle
+ * @size: size of the triangle
+ * Return: void
+*/
+void print_triangle(int size)
+{
+    int lines;
+    int columns;
+    if(size <= 0)
+    {
+        _putchar('\n');
+    }
+    for (lines = 1 ; lines <= size ; lines++)
+    {
+        for (columns = 0 ; columns < lines; columns++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
