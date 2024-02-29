14243c3dabb0d425353c398ce60f296cb8acc832
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-29 10:50:38 +0100

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - entry point
+ * Description: print a string followed by a new line
+ * @str: The character
+ * Return: void
+*/
+void _puts(char *str)
+{
+    int i;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+}
