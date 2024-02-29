229d9a1c2f6976f5d900afb0da55a2fb52f5001c
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-02-29 13:53:06 +0100

    other character

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,10 +5,10 @@
  */
 void puts2(char *str)
 {
-    int i;
-    for (i = 0; str[i] != '\0'; i += 2)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+int i;
+for (i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
