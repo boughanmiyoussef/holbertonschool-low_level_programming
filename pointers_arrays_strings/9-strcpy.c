346829bfdfd76beb8e96dae39fd8f62d51141eec
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-01 10:52:48 +0100

    copy the string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,16 +10,10 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int len, i;
-len = 0;
-while (src[len] != '\0')
+int i = 0;
+while ((dest[i] = src[i]) != '\0')
 {
-len++;
+i++;
 }
-for (i = 0; i < len; i++)
-{
-dest[i] = src[i];
-}
-dest[i] = '\0';
-return (dest);
+return dest;
 }
