5845008aab4c0df993b63e01f3a0cec58c4b9083
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-04 11:10:30 +0100

    memory copy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_mempcy - entry point
+ * Description: function copies n bytes from memory area src to memory area dest
+ * @dest: string Destination
+ * @src: string to be copied
+ * @n: size of the memory 
+ * Return: Pointer To Dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+    while ( src[i] != '\0' && i < n)
+    {
+    dest[i]=src[i];
+    i++;
+    }
+    return (dest);
+}
