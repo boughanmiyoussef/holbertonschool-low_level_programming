d012e24407f2b7a1f99e6fc5ea0c87c4b2214a3d
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-04 11:16:08 +0100

    memory copy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -9,16 +9,11 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
-    while ( src[i] != '\0' && i < n)
-    {
-        dest[i]=src[i];
-        i++;
-    }
-    while (i < n) 
-    {
-     dest[i] = '\0';
-     i++;
-    }
-    return (dest);
+unsigned int i = 0;
+while (i < n)
+{
+dest[i]=src[i];
+i++;
+}
+return (dest);
 }
