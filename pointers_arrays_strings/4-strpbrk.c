088f929bed08a146ecf72bc788b859550927f793
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-04 19:35:21 +0100

    strbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strpbrk - function name
+ * @s: first parameter
+ * @accpet: second parameter
+ * Description: searches a string for any of a set of bytes.
+ * Return : success
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+return(s);
+}
+s++;
+}
+return ('\0');
+}
