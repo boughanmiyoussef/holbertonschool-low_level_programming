e868dd5d3d798e1a6df699a199df94c708d33271
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-04 10:48:19 +0100

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 /**
  * *_memset - entry point
  * @s: string | @b: constant byte | @n: size of the memory
- * Description: fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * Description: fills the first n bytes of the memory area pointed to by s
  * Return: a pointer to the memory area s
 */
 
@@ -11,7 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 unsigned int i;
 for (i = 0; i < n; i++)
 {
-s[i]=b;
+s[i] = b;
 }
-return(s);
+return (s);
 }
