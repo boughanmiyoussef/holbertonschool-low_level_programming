3720ec5928033692f61e769b149b937d780a9714
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-04 19:22:46 +0100

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,15 +8,15 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-    int i;
-    int j;
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; s[i] != accept[j]; j++)
-        {
-            if (accept[j] == '\0')
-            return (i);
-        }
-    }
-    return (0);
+int i;
+int j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; s[i] != accept[j]; j++)
+{
+if (accept[j] == '\0')
+return (i);
+}
+}
+return (0);
 }
