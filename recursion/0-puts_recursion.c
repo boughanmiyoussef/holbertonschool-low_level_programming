7a2ddb1d6c20e60ee355a3e57d3c115e44c8be74
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-06 11:06:00 +0100

    recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - function name
+ * @s: string
+ * Description: prints a string, followed by a new line.
+ * Return: void
+*/
+
+void _puts_recursion(char *s)
+{   
+if (s[0] != '\0')
+{
+_putchar(s[0]);
+_puts_recursion(s + 1);
+} else
+{
+return('\n');
+} 
+}
