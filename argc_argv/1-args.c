73ea907f4e03ea95cc90b99c6d247ea9a6757924
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-08 10:22:33 +0100

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: success
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d", argc - 1); 
+return (0);
+}
