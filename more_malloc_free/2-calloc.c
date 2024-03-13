9278bd728645dfec422e7b656c84607f5dccc76d
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-13 19:42:58 +0100

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - function name
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: return pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *abc;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+abc = malloc(nmemb * size);
+if (abc == NULL)
+return (NULL);
+	
+i = 0;
+while (i < nmemb * size)
+{
+abc[i] = 0;
+i++;
+}
+	
+return (abc);
+}
