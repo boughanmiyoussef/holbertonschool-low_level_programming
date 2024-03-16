f1faa992553711e717115339f65007b2debe6d77
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-16 14:59:53 +0100

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,41 +1,76 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * new_dog - entry point
- * @name: string from main, name of pet
- * @age: number from main, age of pet
- * @owner: string from main, owner of pet
- * Return: p
+ * _strdup - function name
+ * @str:string
+ *
+ * Return: copy
+ */
+char *_strdup(char *str)
+{
+	char *abc;
+	unsigned int length = 0;
+
+	if (str)
+	{
+		while (str[length++])
+			;
+
+		abc = malloc(sizeof(char) * length);
+		if (abc)
+		{
+			while (length--)
+				abc[length] = str[length];
+
+			return (abc);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * new_dog - function name
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *d;
 
-    dog_t *p;
+	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
 
-    p = malloc(sizeof(dog_t));
-    if (p == NULL)
-        return (NULL);
+	if (name)
+	{
+		d->name = _strdup(name);
+		if (!(d->name))
+		{
+			free(d);
+			return (NULL);
+		}
+	}
+	else
+		d->name = NULL;
 
-    p->name = malloc(strlen(name) + 1);
-    if (p->name == NULL)
-    {
-        free(p);
-        return (NULL);
-    }
-    strcpy(p->name, name);
+	d->age = age;
 
-    p->owner = malloc(strlen(owner) + 1);
-    if (p->owner == NULL)
-    {
-        free(p->name);
-        free(p);
-        return (NULL);
-    }
-    strcpy(p->owner, owner);
+	if (owner)
+	{
+		d->owner = _strdup(owner);
+		if (!(d->owner))
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+	}
+	else
+		d->owner = NULL;
 
-    p->age = age;
-
-    return (p);
+	return (d);
 }
