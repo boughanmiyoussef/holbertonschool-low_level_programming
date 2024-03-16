b687596ef1c3c874552bfc69cbb8fd865033f726
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-16 14:50:00 +0100

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,26 +10,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *p;
-	
+
+    dog_t *p;
+
     p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+    if (p == NULL)
+        return NULL;
+
+    p->name = malloc(strlen(name) + 1);
+    if (p->name == NULL)
+    {
+        free(p);
+        return NULL;
+    }
+    strcpy(p->name, name);
+
+    p->owner = malloc(strlen(owner) + 1);
+    if (p->owner == NULL)
+    {
+        free(p->name);
+        free(p);
+        return NULL;
+    }
+    strcpy(p->owner, owner);
+
+    p->age = age;
+
+    return p;
 }
