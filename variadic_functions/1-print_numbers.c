bf91b63ed152ac14a43672641a306e43e7a40207
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-03-23 16:21:30 +0100

    variadic

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,10 +22,10 @@ va_start(list, n);
 
 if (n > 0)
 printf("%d", va_arg(list, int));
-	
+
 for (i = 1; i < n; i++)
 printf("%s%d", sep, va_arg(list, int));
-	
+
 printf("\n");
 va_end(list);
 }
--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,23 +15,23 @@ va_list list;
 
 if (separator == NULL || *separator == 0)
 sep = "";
-	
+
 else
 sep = (char *) separator;
 va_start(list, n);
 
 if (n > 0)
 printf("%s", va_arg(list, char *));
-	
+
 for (i = 1; i < n; i++)
 {
 ptr = va_arg(list, char*);
-		
+	
 if (ptr == NULL)
 ptr = "(nil)";
 printf("%s%s", sep, ptr);
 }
-	
+
 printf("\n");
 va_end(list);
 }
