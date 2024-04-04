0eca97629b1175f4a627dfce82362bb9cb1a35f7
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-04-04 20:40:40 +0100

    commit

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,30 +1,31 @@
 #include "lists.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
-  * add_node - function name
-  *
-  * @head: pointer
-  * @str: string
-  * Return: address of new element
-  */
+ * add_node - function naame
+ * @head: pointer to head of list
+ * @str: string
+ * Return: address of new node or NULL
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *abc;
 
-if (!(head))
+if (!head || !str)
 return (NULL);
 
 abc = malloc(sizeof(list_t));
-if (abc == NULL)
+if (!abc)
 return (NULL);
-	
-if (str)
+
 abc->str = strdup(str);
-	
+if (!abc->str)
+{
+free(abc);
+return (NULL);
+}
+
+abc->len = _strlen(abc->str);
 abc->next = *head;
 *head = abc;
-	
 return (abc);
 }
