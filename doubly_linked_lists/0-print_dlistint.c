6420e7fc51f5d5224403b29869f55dd2a4728d99
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-04-04 21:12:19 +0100

    commit

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - function name
+ * @h: the head of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t nodes = 0;
+
+if (!h)
+return (0);
+
+while (h)
+{
+printf("%i\n", h->n);
+nodes++;
+h = h->next;
+}
+	
+return (nodes);
+}
