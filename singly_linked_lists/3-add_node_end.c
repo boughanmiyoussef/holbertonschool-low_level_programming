9106d14be94471f9e4cde15e28deadf7cb27edb5
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-04-04 20:53:29 +0100

    commit

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,36 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node_end - function name
- * @head: pointer to the head of list
- * @str: string
- * Return: address of new node 
+ * @head: pointer to pointer to head of list
+ * @str: string to initialize node 
+ * Return: address of new node, or NULL if malloc failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *abc;
-list_t *def = *head;
+size_t i = 0;
+list_t *xyz = *head;
+list_t *abc = malloc(sizeof(list_t));
 
-if (!head || !str)
+if (abc == NULL)
 return (NULL);
-
-abc = malloc(sizeof(list_t));
-if (!abc)
-return (NULL);
-
-abc->str = strdup(str);
-if (!abc->str)
-{
-free(abc);
-return (NULL);
-}
-
-abc->len = _strlen(abc->str);
+	
 abc->next = NULL;
+abc->str = strdup(str);
+	
+if (abc->str)
+while (str[i])
+++i;
+abc->len = i;
 
-if (def)
+if (xyz)
 {
-while (def->next)
-def = def->next;
-def->next = abc;
+while (xyz->next)
+xyz = xyz->next;
+xyz->next = abc;
 }
+	
 else
 *head = abc;
 return (abc);
