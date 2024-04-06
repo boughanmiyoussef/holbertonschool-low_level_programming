4b2e37fc7124c69bd18900bf9d5d781acc122562
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-04-06 11:17:26 +0100

    doubly linked lists

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - function name
+ * @h: Pointer 
+ * @n: integer
+ * @idx: Position
+ * Return: The direction of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *abc, *xyz = *h;
+
+if (idx == 0)
+return (add_dnodeint(h, n));
+
+for (; idx != 1; idx--)
+{
+if (xyz == NULL)
+return (NULL);
+xyz = xyz->next;
+}
+
+if (xyz->next == NULL)
+return (add_dnodeint_end(h, n));
+
+abc = malloc(sizeof(dlistint_t));
+
+if (abc == NULL)
+return (NULL);
+
+abc->n = n;
+abc->next = xyz->next;
+abc->prev = xyz;
+xyz->next->prev = abc;
+xyz->next = abc;
+
+return (abc);
+}
