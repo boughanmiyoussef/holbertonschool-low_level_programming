caa137a9f757290da12b8ddac693eb5ed5fdf00a
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-04-06 22:08:42 +0100

    file_io

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -2,19 +2,17 @@
 
 /**
  * append_text_to_file - function name
- * @filename: variable pointer
- * @text_content: content file
- * Description: function that appends text at the end of a file
+ * @filename:pointer
+ * @text_content: text
+ * Description: function that appends text at the end of a abc
  * Return: 1 on success, -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
 int i = 0, abc;
-
 if (filename == NULL)
 return (-1);
-
 if (text_content == NULL)
 text_content = "";
 
@@ -23,11 +21,8 @@ while (text_content[i] != '\0')
 i++;
 }
 abc = open(filename, O_WRONLY | O_APPEND);
-
 if (abc == -1)
 return (-1);
-
 write(abc, text_content, i);
-
 return (1);
 }
