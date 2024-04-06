a30e21b4bb78f98f36b884665e8a2f35d5c5974a
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-04-06 11:08:04 +0100

    doubly linked lists

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - function name
+ * @head: list
+ * @n: number
+ * Return: new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new, *first;
+
+first = *head;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+	
+if (*head == NULL)
+{
+new->next = NULL;
+new->n = n;
+new->prev = NULL;
+*head = new;
+return (new);
+}
+while (first->next)
+{
+first = first->next;
+}
+first->next = new;
+new->prev = first;
+new->next = NULL;
+new->n = n;
+return (new);
+}
